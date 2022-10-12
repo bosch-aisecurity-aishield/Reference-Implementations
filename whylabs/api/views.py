6872678bb72acf974f51a544b94bec4aa2eab17a
MVB3KOR <Avinash.Amballa@in.bosch.com>
2022-10-12 20:34:25 +0530

    Revert "whylogs 1.1 version"
    This reverts commit a4a8ac8fdf69bb0e6e3eb7242784180a81ff986b.

--- whylabs/api/views.py
+++ whylabs/api/views.py
@@ -1,18 +1,14 @@
 """ View functions for every endpoint. """
 import app
 import numpy as np
-from api.utils import get_prediction_iris, get_prediction_mnist , prenet_iris , prenet_mnist
+from api.utils import get_prediction_iris, get_prediction_mnist, initialize_logger , prenet_iris , prenet_mnist
 from flask import Blueprint, request
 from flask_pydantic import validate
 from schemas import FeatureVector_IRIS , FeatureVector_MNIST
 from utils import object_response
 
-import whylogs as why
-import datetime
-from whylogs.api.writer.whylabs import WhyLabsWriter
-writer = WhyLabsWriter()
-
 blueprint = Blueprint("api", __name__, url_prefix="/api/v1")
+initialize_logger()
 
 
 @blueprint.route("/health", methods=["GET"])
@@ -50,18 +46,10 @@ def predict(body : FeatureVector_MNIST):  #body is a dict
     if def_out == 1 :
         pred = "A"
 
-    # select timestamp
-    current_date = datetime.datetime.utcnow() - datetime.timedelta(days=2)
-
-    # log prenet output    
-    profile = why.log({"attack": def_out}).profile()
-    profile.set_dataset_timestamp(current_date)
-    writer.write(file=profile.view())
-
+    # log prenet output
+    app.whylabs_logger.log({"attack": def_out})
 
     # Log predicted class
-    profile = why.log({"class": pred}).profile()
-    profile.set_dataset_timestamp(current_date)
-    writer.write(file=profile.view())
+    app.whylabs_logger.log({"class": pred})
 
     return object_response({"class": pred,"attack": def_out}, 200)
